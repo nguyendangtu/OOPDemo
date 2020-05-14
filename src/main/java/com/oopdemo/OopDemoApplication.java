88b98e4a3113063b23975f8227aed220824b943b
JOHNNGUYEN <nguyendangtu1987@gmail.com>
2020-05-14 08:09:14 +0800

    init demo oop project

---
+++ src/main/java/com/oopdemo/OopDemoApplication.java
@@ -0,0 +1,13 @@
+package com.oopdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oopdemo/OopDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
